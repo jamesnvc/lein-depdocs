2af6e2dcc980c820b2b002170dd0f666466cd929
James N. V. Cash <james.nvc@gmail.com>
2017-04-01 21:25:46 -0400

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-depdocs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Leiningen plugin for automatically generating usage documentation"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
